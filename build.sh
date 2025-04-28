0d89e0d153ba03c429b6c59331939331a0f347d8
suhong99 <topic828@naver.com>
2025-04-28 16:14:21 +0900

    쉘수정

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./test/* ./output
-cp -R ./output ./build_orga
+
+# 일반 파일 복사 (build.sh 제외하고 복사)
+rsync -av --exclude 'build.sh' ./test/ ./output/
+
+# 숨김 파일 복사 (.gitignore 등)
+cp ./test/.gitignore ./output/
+
+# output -> 다시 test로 복사
+cp -R ./output/* ./test/

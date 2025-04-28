49b89316e52056f4ca009ca5de5438e9fc79d28c
suhong99 <topic828@naver.com>
2025-04-28 17:11:24 +0900

    테스트

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ../
-mkdir -p output
+mkdir output
 
 # 1. test 디렉토리의 모든 파일과 디렉토리를 output으로 복사 (숨김 파일 제외)
-cp -R ./test/* ./output/
+cp -R ./test/* ./output
 
 # 2. .gitignore만 복사
-cp ./test/.gitignore ./output/
+cp ./test/.gitignore ./output
 
 # 3. output 디렉토리에서 build.sh 파일 제거
 rm -f ./output/build.sh
